1efe5d27b34e0c52700fa0b7ced852442dbd110d
paulgrn <paulgrn@hotmail.com>
2016-12-23 17:59:30 -0600

    Second whole push

--- Scrabble/Form1.cs
+++ Scrabble/Form1.cs
@@ -16,5 +16,10 @@ namespace Scrabble
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
